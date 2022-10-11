3fec119e1c33dd3e9b196b4461abf752b2b2b901
tathusphol <63070065@kmitl.ac.th>
2022-10-11 15:13:36 +0700

    Initial commit

---
+++ src/main/java/com/sop/chapter6/testservice/TestServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sop.chapter6.testservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+
+@SpringBootApplication
+public class TestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServiceApplication.class, args);
+	}
+
+}
